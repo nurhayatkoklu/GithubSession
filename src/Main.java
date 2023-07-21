eda33b82b7a16689cd4bef9d4cabf4a2d9bea0ea
JazeeraArinova <canarijaz@gmail.com>
2023-07-21 13:27:55 -0400

    Jazeeras 1st commit

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Nurhayat");
+        System.out.println("Jazeera was here");
 
     }
 }
